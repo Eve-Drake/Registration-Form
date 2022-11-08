c452a92f8c79155abc34ef44cb862537bda892b3
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-08 12:07:32 +0000

    Validation

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,36 +1,52 @@
-import {useRef, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import { BiInfoCircle, BiCheck, BiX } from "react-icons/bi";
 
-const REGEX_USERNAME = /^[a-z0-9_-]{3,16}$/
-const REGEX_PASSWORD = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
+
+const REGEX_USERNAME = /^[a-zA-Z0-9_-]{3,16}$/
+const REGEX_PASSWORD = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 
 const Register = () => {
-    const userRef = useRef();
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const errorRef = useRef();
+
     const [username, setUsername] = useState<string>('')
     const [validUserame, setValidUsername] = useState<boolean>(false);
-    const [password, setPassword] = useState<string>('')
-    const [validPassword, setValidPassword] = useState<boolean>(false)
-    const [secondPassword, setSecondPassword] = useState<string>('')
-    const [match, setMatch] = useState<boolean>(false)
+    const [userFocus, setUserFocut] = useState<boolean>(false)
 
-    const testValidUsername =()=>{
+    const [password, setPassword] = useState<string>('')
+    const [secondPassword, setSecondPassword] = useState<string>('')
+    const [matchching, setMatching] = useState<boolean>(false)
+    const [validPassword, setValidPassword] = useState<boolean>(false)
+    const [passwordFocus, setPasswordFocus] = useState<boolean>(false)
+
+    const [error, setError] = useState<boolean>(false)
+
+    useEffect(()=>{
+      if (usernameRef.current){
+        usernameRef.current.focus()
+      }
+    },[])
+
+    useEffect(()=>{
       if(REGEX_USERNAME.test(username)){
         setValidUsername(true)
       }
       else{
         setValidUsername(false)
       }
-    }
+    },[username])
 
-    const testValidPassword =()=>{
-      if(REGEX_PASSWORD.test(username)){
-        setValidUsername(true)
+    useEffect(()=>{
+      if(REGEX_PASSWORD.test(password)){
+        setValidPassword(true)
       }
       else{
-        setValidUsername(false)
+        setValidPassword(false)
       }
-    }
-    }
+      if(password === secondPassword){
+        setMatching(true)
+      }
+    },[password])
 
   return (
     <div>
@@ -45,14 +61,16 @@ const Register = () => {
         value={password}
         onChange={(e)=>setPassword(e.target.value)}
        />
-       <input 
-        placeholder='Repeat your Password'
+
+      <input 
+        placeholder='Enter your Password'
         value={secondPassword}
         onChange={(e)=>setSecondPassword(e.target.value)}
        />
-       <button onClick={()=>handleClick}>Register</button>
+  
+       <button>Register</button>
     </div>
   )
-}
+  }
 
 export default Register
